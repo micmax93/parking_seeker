9fc89d5a23c97936422fbf680e6bdc07d661063b
micmax93 <m.elfartas@gmail.com>
2015-06-10 19:54:05 +0200

    Fixed schema.

--- build_db.py
+++ build_db.py
@@ -1,11 +1,11 @@
-__author__ = 'se416237'
+__author__ = "se416237"
 from cassandra.cluster import Cluster
 
-cluster = Cluster(['micmax93.me'])
+cluster = Cluster(["micmax93.me"])
 conn = cluster.connect()
 conn.execute("CREATE KEYSPACE IF NOT EXISTS GPS WITH REPLICATION = { 'class' : 'SimpleStrategy', 'replication_factor' : 4 }")
-conn.execute('USE GPS')
+conn.execute("USE GPS")
 
-conn.execute('CREATE TABLE IF NOT EXISTS parkings (id varchar,name varchar,address varchar,lat float,lon float,capacity int,PRIMARY KEY (id))')
-conn.execute('CREATE TABLE IF NOT EXISTS users (username varchar,parking varchar,slot_no int,PRIMARY KEY (username))')
-conn.execute('CREATE TABLE IF NOT EXISTS slots (parking_id varchar,slot_no int,user varchar,PRIMARY KEY ( parking_id, slot_no))')
+conn.execute("CREATE TABLE IF NOT EXISTS parkings (id varchar,name varchar,address varchar,lat float,lon float,capacity int,PRIMARY KEY (id))")
+conn.execute("CREATE TABLE IF NOT EXISTS users (username varchar,parking varchar,slot_no int,PRIMARY KEY (username))")
+conn.execute("CREATE TABLE IF NOT EXISTS slots (parking_id varchar,slot_no int,user varchar,PRIMARY KEY ( parking_id, slot_no))")
